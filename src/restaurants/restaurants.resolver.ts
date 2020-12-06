6afe597c9223af4e43574ebe3907be405d98c10b
tarask <tarask.maxs@gmail.com>
2020-12-06 15:18:39 +0200

    add examples of creating at scheme with ts

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,22 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver((of) => Restaurant)
+export class RestaurantsResolver {
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant {
+    console.log('======>>', veganOnly);
+
+    return null;
+  }
+
+  @Mutation((returns) => Boolean)
+  createRestaurant(
+    @Args() createRestaurantDto: CreateRestaurantDto,
+  ): boolean {
+    console.log('========>>>>>>input', createRestaurantDto);
+
+    return true;
+  }
+}
